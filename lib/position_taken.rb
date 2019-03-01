998d255d68f6e07dbf6b81362abf5b64762574e5
Michael Carroll <carrollm2@gmail.com>
2019-03-01 02:11:00 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+def position_taken?(gets)
+  if board[gets.strip] == " " || board[gets.strip] == "" || board[gets.strip] = nil
+    return false
+  else
+    return true
+  end
+
+end
